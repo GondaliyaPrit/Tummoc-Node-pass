4a312deb0ab5e2991a760eb3ddb65e710298d69a
Prit Gondaliya <prit.github@gmail.com>
2022-09-06 17:16:10 +0530

    first commit

---
+++ dummydata.js
@@ -0,0 +1,4 @@
+const UserDetails = require("./user");
+
+UserDetails.register({ username: "prit", active: false }, "prit");
+UserDetails.register({ username: "gondaliya", active: false }, "gondaliya");
